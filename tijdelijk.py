ab75e8e386546b0502a73ff856872850aea2e932
CEMJ De Backer <cemjdebacker@telenet.be>
2024-12-27 14:34:16 +0100

    bestand tijdelijk.py gecodeerd

--- tijdelijk.py
+++ tijdelijk.py
@@ -1,16 +1,24 @@
+# Maak een dictionary Prijzen:
 prijzen = {"aardbei" : 3,
            "vanille" : 4,
            "chocolade": 5}
 
+# Voeg een variabele met naam "aanbieding" toe (0.8 van prijzen voor aardbei)
 aanbieding = prijzen["aardbei"]*0.8
 
+# Reclame tekst
 reclame_tekst = f"Vandaag in de aanbieding: aardbei-ijs, 1 liter - slechts € {aanbieding}"
+
+# Reclame tekst 2 - waarbij decimalen in aanbieding worden gereduceerd
 reclame_tekst2 = reclame_tekst[:62]
 
+# Reclame tekst 3 - alles in hoofdletters
 reclame_tekst3 = reclame_tekst2.upper()
 
+# Reclame tekst 4 - list
 reclame_tekst4 = reclame_tekst3.split()
 
+# Alle woorden onder elkaar - geen hoofdletters
 for e1 in reclame_tekst4:
     if len(e1)>=5:
         print(e1.upper())
