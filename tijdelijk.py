b652fa34bf44d32288d7241e45a9e81be5616681
CEMJ De Backer <cemjdebacker@telenet.be>
2024-12-28 16:01:45 +0100

    functies togevoegd

--- tijdelijk.py
+++ tijdelijk.py
@@ -1,26 +1,33 @@
-# Maak een dictionary prijzen:
-prijzen = {"aardbei" : 3,
-           "vanille" : 4,
-           "chocolade": 5}
+from helper import decoreer
 
-# Voeg een variabele met naam "aanbieding" toe (0.8 van prijzen voor aardbei)
-aanbieding = prijzen["aardbei"]*0.8
+def print_aanbieding():
+    # Maak een dictionary prijzen:
+    prijzen = {"aardbei" : 3,
+            "vanille" : 4,
+            "chocolade": 5}
 
-# Reclame tekst
-reclame_tekst = f"Vandaag in de aanbieding: aardbei-ijs, 1 liter - slechts € {aanbieding}"
+    # Voeg een variabele met naam "aanbieding" toe (0.8 van prijzen voor aardbei)
+    aanbieding = prijzen["aardbei"]*0.8
 
-# Reclame tekst 2 - waarbij decimalen in aanbieding worden gereduceerd
-reclame_tekst2 = reclame_tekst[:62]
+    # Reclame tekst
+    reclame_tekst = f"Vandaag in de aanbieding: aardbei-ijs, 1 liter - slechts € {aanbieding}"
 
-# Reclame tekst 3 - alles in hoofdletters
-reclame_tekst3 = reclame_tekst2.upper()
+    # Reclame tekst 2 - waarbij decimalen in aanbieding worden gereduceerd
+    reclame_tekst2 = reclame_tekst[:62]
 
-# Reclame tekst 4 - list
-reclame_tekst4 = reclame_tekst3.split()
+    # Reclame tekst 3 - alles in hoofdletters
+    reclame_tekst3 = reclame_tekst2.upper()
 
-# Alle woorden onder elkaar - enkel 5 letter of meer hoofdletters rest kleine letters
-for e1 in reclame_tekst4:
-    if len(e1)>=5:
-        print(e1.upper())
-    else: 
-        print(e1.lower())
+    # Reclame tekst 4 - list
+    reclame_tekst4 = reclame_tekst3.split()
+
+    # Alle woorden onder elkaar - enkel 5 letter of meer hoofdletters rest kleine letters
+    for e1 in reclame_tekst4:
+        if len(e1)>=5:
+            print(e1.upper())
+        else: 
+            print(e1.lower())
+
+decoreer("Aanbieding")
+
+print_aanbieding()
